db7955aa63279269519ac675bc5b094b41b4790d
Brodaxer <115712630+Brodaxer@users.noreply.github.com>
2023-02-10 15:59:23 +0100

    Add files via upload

--- HospitalApp.java
+++ HospitalApp.java
@@ -2,14 +2,14 @@ class HospitalApp {
     public static void main(String[] args) {
         Person doc = new Doctor("lol","dow",123555,111);
         Person nurs = new Nurse("dog","nog",23444,12);
-        Person nurs2 = new Nurse("dog","nog",23444,12);
+        Person nurs2 = new Nurse("cwlk","nblok",33344,132);
 
         Hospital hospital = new Hospital();
-        hospital.add(doc);
+        hospital.add(new Doctor("lol","dow",123555,111)); //tyz dziala
         hospital.add(nurs);
         hospital.add(nurs2);
 
         System.out.println(hospital.getInfo());
-        
+
     }
 }
